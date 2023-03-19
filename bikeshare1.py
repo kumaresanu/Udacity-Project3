8def1eaa778ac71853c1537c8bfb3e2ebef74205
Kumaresanu <kumaresan.u@hotmail.com>
2023-03-19 21:51:35 +0800

    refactor: Added a comment line in the code

--- bikeshare1.py
+++ bikeshare1.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import json
 
+
+#add
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york': 'new_york_city.csv',
              'washington': 'washington.csv'}
